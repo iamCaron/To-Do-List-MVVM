7c992a0e2135441155279d4519ff2e33ac765fb9
Caron Fernandes <caronf82@gmail.com>
2020-01-06 17:57:27 +0530

    Project Files Added to the Repository

---
+++ app/src/main/java/com/caron/mvvmexample/Note.java
@@ -0,0 +1,43 @@
+package com.caron.mvvmexample;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "note_table")
+public class Note {
+
+    @PrimaryKey(autoGenerate = true)
+    private int d;
+
+    private String title;
+
+    private String description;
+
+    private int priority;
+
+    public Note(String title, String description, int priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public int getD() {
+        return d;
+    }
+
+    public void setD(int d) {
+        this.d = d;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
